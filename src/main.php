cdfdc08f838b9c884f0215ca7f3b6d849e21d322
Mikhail Kornaukhov <mkornaukhov03@yandex.ru>
2024-10-13 17:45:39 +0300

    apply formatter

--- src/main.php
+++ src/main.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 /**
  * Messenger functionality:
@@ -10,25 +10,27 @@
  * TODO
  */
 
-class Message {
+class Message
+{
     public int $user_id;
     public int $timestamp;
     public string $text;
 
-    public function __construct(int $user_id, string $text) {
+    public function __construct(int $user_id, string $text)
+    {
         $this->user_id = $user_id;
         $this->text = $text;
     }
 
     /** @returns Message */
-    public static function fromJson(string $json) {
+    public static function fromJson(string $json)
+    {
         return JsonEncoder::decode($json, Message::class);
     }
 };
 
-
-
-function main() {
+function main()
+{
     $uri = $_SERVER['REQUEST_URI'];
     var_dump($uri);
     echo "-------------\n";
